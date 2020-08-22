1aad4054dacead1803e5ee94a9850969230628fa
mossnana <samurai.hanso@gmail.com>
2020-08-22 20:02:22 +0700

    demo basic

---
+++ src/bar.js
@@ -0,0 +1,5 @@
+function bar() {
+    console.log('Hello Webpack')
+}
+
+module.exports = bar
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import bar from './bar';
+import './css/webpack-css.css'
+
+bar();
+
+document.write(`
+    <h1 id='webpack-css'> Hello CSS Loader </h1>
+`)
